489d15507fd0356eda8a76e0924416e25318bc5d
dnzhelloo <dnzhelloo@icloud.com>
2023-06-16 19:32:42 +0300

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Deniz on 16.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            Image(systemName: "globe").imageScale(.large).foregroundColor(.blue)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
